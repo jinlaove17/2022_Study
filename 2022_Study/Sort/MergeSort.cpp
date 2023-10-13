8c9a6e4aa4e91b1c42b625cc439c4af78ef09748
JongWoo <jinlaove17@naver.com>
2023-10-14 01:59:37 +0900

    [정렬] 합병 정렬 복습

--- 2022_Study/Sort/MergeSort.cpp
+++ 2022_Study/Sort/MergeSort.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
 #include <vector>
+#include <functional>
 
 using namespace std;
 
+struct Less
+{
+	bool operator()(int a, int b)
+	{
+		// stable: <=
+		return a <= b;
+	}
+};
+
+struct Greater
+{
+	bool operator()(int a, int b)
+	{
+		// stable: <=
+		return a >= b;
+	}
+};
+
 void PrintArray(const string& tag, const vector<int>& v);
-void MergeSort(vector<int>& v, int begin, int end);
-void Merge(vector<int>& v, int begin, int mid, int end);
+void MergeSort(vector<int>& v, int begin, int end, function<bool(int, int)> callable = Less());
+void Merge(vector<int>& v, const function<bool(int, int)>& callable, int begin, int mid, int end);
 
 int main()
 {
@@ -14,7 +33,7 @@ int main()
 	cout << "[합병 정렬]" << endl << endl;
 
 	PrintArray("▶ 정렬 전", v);
-	MergeSort(v, 0, v.size() - 1);
+	MergeSort(v, 0, v.size() - 1); // MergeSort(v, 0, v.size() - 1, Greater());
 	PrintArray("▶ 정렬 후", v);
 }
 
@@ -24,60 +43,55 @@ void PrintArray(const string& tag, const vector<int>& v)
 
 	for (int i = 0; i < v.size(); ++i)
 	{
-		cout << v[i] << " ";
+		cout << v[i] << ' ';
 	}
 
 	cout << endl << endl;
 }
 
-void MergeSort(vector<int>& v, int begin, int end)
+void MergeSort(vector<int>& v, int begin, int end, function<bool(int, int)> callable)
 {
 	if (begin < end)
 	{
 		int mid = (begin + end) / 2;
 
-		MergeSort(v, begin, mid);
-		MergeSort(v, mid + 1, end);
-		Merge(v, begin, mid, end);
+		MergeSort(v, begin, mid, callable);
+		MergeSort(v, mid + 1, end, callable);
+		Merge(v, callable, begin, mid, end);
 	}
 }
 
-void Merge(vector<int>& v, int begin, int mid, int end)
+void Merge(vector<int>& v, const function<bool(int, int)>& callable, int begin, int mid, int end)
 {
-	vector<int> sorted(v.size());
+	vector<int> tmp(v.size());
 	int i = begin;
 	int j = mid + 1;
-	int k = i;
+	int k = begin;
 
-	while (i <= mid && j <= end)
+	while ((i <= mid) && (j <= end))
 	{
-		if (v[i] <= v[j])
+		if (callable(v[i], v[j]))
 		{
-			sorted[k++] = v[i++];
+			tmp[k++] = v[i++];
 		}
 		else
 		{
-			sorted[k++] = v[j++];
+			tmp[k++] = v[j++];
 		}
 	}
 
-	if (i <= mid)
+	while (i <= mid)
 	{
-		for (; i <= mid; ++i)
-		{
-			sorted[k++] = v[i];
-		}
+		tmp[k++] = v[i++];
 	}
-	else
+
+	while (j <= end)
 	{
-		for (; j <= end; ++j)
-		{
-			sorted[k++] = v[j];
-		}
+		tmp[k++] = v[j++];
 	}
 
 	for (int l = begin; l <= end; ++l)
 	{
-		v[l] = sorted[l];
+		v[l] = tmp[l];
 	}
 }
