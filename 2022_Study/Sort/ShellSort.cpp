3c24b4111fdbfb40c75943775f402b1a4401cbd0
JongWoo <jinlaove17@naver.com>
2023-10-14 01:36:45 +0900

    [정렬] 셸 정렬 복습

--- 2022_Study/Sort/ShellSort.cpp
+++ 2022_Study/Sort/ShellSort.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
 #include <vector>
+#include <functional>
 
 using namespace std;
 
+struct Less
+{
+	bool operator()(int a, int b)
+	{
+		return a < b;
+	}
+};
+
+struct Greater
+{
+	bool operator()(int a, int b)
+	{
+		return a > b;
+	}
+};
+
 void PrintArray(const string& tag, const vector<int>& v);
-void ShellSort(vector<int>& v);
-void InsertionSort(vector<int>& v, int begin, int end, int gap);
+void ShellSort(vector<int>& v, function<bool(int, int)> callable = Less());
+void InsertionSort(vector<int>& v, const function<bool(int, int)>& callable, int begin, int end, int gap);
 
 int main()
 {
@@ -14,7 +31,7 @@ int main()
 	cout << "[셸 정렬]" << endl << endl;
 
 	PrintArray("▶ 정렬 전", v);
-	ShellSort(v);
+	ShellSort(v); // ShellSort(v, Greater());
 	PrintArray("▶ 정렬 후", v);
 }
 
@@ -24,15 +41,17 @@ void PrintArray(const string& tag, const vector<int>& v)
 
 	for (int i = 0; i < v.size(); ++i)
 	{
-		cout << v[i] << " ";
+		cout << v[i] << ' ';
 	}
 
 	cout << endl << endl;
 }
 
-void ShellSort(vector<int>& v)
+void ShellSort(vector<int>& v, function<bool(int, int)> callable)
 {
-	for (int gap = v.size() / 2; gap > 0; gap /= 2)
+	int n = static_cast<int>(v.size());
+
+	for (int gap = n / 2; gap > 0; gap /= 2)
 	{
 		if (gap % 2 == 0)
 		{
@@ -41,22 +60,31 @@ void ShellSort(vector<int>& v)
 
 		for (int i = 0; i < gap; ++i)
 		{
-			InsertionSort(v, i, v.size() - 1, gap);
+			InsertionSort(v, callable, i, n - 1, gap);
 		}
 	}
 }
 
-void InsertionSort(vector<int>& v, int begin, int end, int gap)
+void InsertionSort(vector<int>& v, const function<bool(int, int)>& callable, int begin, int end, int gap)
 {
+	int n = static_cast<int>(v.size());
+
 	for (int i = begin + gap, j = 0; i <= end; i += gap)
 	{
-		int key = v[i];
+		int tmp = v[i];
 
-		for (j = i - gap; j >= begin && key < v[j]; j -= gap)
+		for (j = i - gap; j >= begin; j -= gap)
 		{
-			v[j + gap] = v[j];
+			if (callable(tmp, v[j]))
+			{
+				v[j + gap] = v[j];
+			}
+			else
+			{
+				break;
+			}
 		}
 
-		v[j + gap] = key;
+		v[j + gap] = tmp;
 	}
 }
