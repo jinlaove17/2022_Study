64f31583d3c4e6e4b24f6e08e85f966d1b0117a2
JongWoo <jinlaove17@naver.com>
2023-10-15 02:05:16 +0900

    [정렬] 퀵 정렬 복습

--- 2022_Study/Sort/QuickSort.cpp
+++ 2022_Study/Sort/QuickSort.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 #include <vector>
+#include <functional>
 
 using namespace std;
 
+struct Less
+{
+	bool operator()(int a, int b)
+	{
+		return a <= b;
+	}
+};
+
+struct Greater
+{
+	bool operator()(int a, int b)
+	{
+		return a >= b;
+	}
+};
+
 void PrintArray(const string& tag, const vector<int>& v);
-void QuickSort(vector<int>& v, int begin, int end);
+void QuickSort(vector<int>& v, int begin, int end, function<bool(int, int)> callable = Less());
 
 int main()
 {
@@ -13,7 +30,7 @@ int main()
 	cout << "[퀵 정렬]" << endl << endl;
 
 	PrintArray("▶ 정렬 전", v);
-	QuickSort(v, 0, v.size() - 1);
+	QuickSort(v, 0, v.size() - 1); // QuickSort(v, 0, v.size() - 1, Greater());
 	PrintArray("▶ 정렬 후", v);
 }
 
@@ -23,13 +40,13 @@ void PrintArray(const string& tag, const vector<int>& v)
 
 	for (int i = 0; i < v.size(); ++i)
 	{
-		cout << v[i] << " ";
+		cout << v[i] << ' ';
 	}
 
 	cout << endl << endl;
 }
 
-void QuickSort(vector<int>& v, int begin, int end)
+void QuickSort(vector<int>& v, int begin, int end, function<bool(int, int)> callable)
 {
 	if (begin < end)
 	{
@@ -39,25 +56,24 @@ void QuickSort(vector<int>& v, int begin, int end)
 
 		while (left <= right)
 		{
-			while (left <= end && v[left] < v[pivot])
+			while ((left <= end) && (callable(v[left], v[pivot])))
 			{
 				++left;
 			}
 
-			while (right >= begin && v[right] > v[pivot])
+			while ((right > begin) && (callable(v[pivot], v[right])))
 			{
 				--right;
 			}
 
-			if (left <= right)
+			if (left < right)
 			{
 				swap(v[left], v[right]);
 			}
 		}
 
 		swap(v[pivot], v[right]);
-
-		QuickSort(v, begin, right);
-		QuickSort(v, left, end);
+		QuickSort(v, begin, right - 1, callable);
+		QuickSort(v, right + 1, end, callable);
 	}
 }
