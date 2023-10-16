1384377e1aa6b8d5ce787fc7bb0b39623da7c55a
JongWoo <jinlaove17@naver.com>
2023-10-16 13:42:04 +0900

    [정렬] 힙 정렬 복습

--- 2022_Study/Sort/HeapSort.cpp
+++ 2022_Study/Sort/HeapSort.cpp
@@ -1,65 +1,82 @@
 #include <iostream>
 #include <vector>
+#include <functional>
+#include <cassert>
 
 using namespace std;
 
-template<typename T>
+struct Less
+{
+	bool operator()(int a, int b)
+	{
+		return a < b;
+	}
+};
+
+struct Greater
+{
+	bool operator()(int a, int b)
+	{
+		return a > b;
+	}
+};
+
+// STL의 priority_queue처럼 Default는 Less를 사용하는 최대힙이고, Greater를 사용하여 최소힙으로 만들 수 있도록 구현하였다.
+// m_elemenets의 인덱스는 0부터 시작한다.
+template<typename T, typename Callable = Less>
 class CHeap
 {
 private:
-	vector<T> m_heap;
+	vector<T> m_elements;
 
 public:
-	CHeap() = default;
-	~CHeap() = default;
-
-	void Insert(T n)
+	void Insert(const T& val)
 	{
-		m_heap.push_back(n);
+		m_elements.push_back(val);
 
-		int i = m_heap.size() - 1;
-		int parent = (i - 1) / 2;
+		int cur = static_cast<int>(m_elements.size()) - 1;
+		int parent = (cur - 1) / 2;
 
-		while (i > 0 && m_heap[i] < m_heap[parent])
+		while ((parent >= 0) && Callable()(m_elements[parent], m_elements[cur]))
 		{
-			swap(m_heap[i], m_heap[parent]);
-
-			i = parent;
-			parent = (i - 1) / 2;
+			swap(m_elements[cur], m_elements[parent]);
+			cur = parent;
+			parent = (cur - 1) / 2;
 		}
 	}
 
 	T Delete()
 	{
-		T root = m_heap.front();
-		T last = m_heap.back();
+		assert(m_elements.size() > 0);
 
-		m_heap.pop_back();
+		T root = move(m_elements.front());
+		T last = move(m_elements.back());
 
-		if (!m_heap.empty())
+		m_elements.pop_back();
+
+		if (!m_elements.empty())
 		{
 			int parent = 0;
 			int child = 1;
 
-			while (child < m_heap.size())
+			while (child < m_elements.size())
 			{
-				if (child < m_heap.size() - 1 && m_heap[child + 1] < m_heap[child])
+				if ((child < m_elements.size() - 1) && Callable()(m_elements[child], m_elements[child + 1]))
 				{
 					++child;
 				}
 
-				if (last <= m_heap[child])
+				if (Callable()(m_elements[child], last))
 				{
 					break;
 				}
 
-				m_heap[parent] = m_heap[child];
-
+				m_elements[parent] = m_elements[child];
 				parent = child;
-				child = 2 * parent;
+				child = 2 * parent + 1;
 			}
 
-			m_heap[parent] = last;
+			m_elements[parent] = last;
 		}
 
 		return root;
@@ -76,16 +93,16 @@ int main()
 
 	PrintArray("▶ 정렬 전", v);
 
-	CHeap<int> heap;
+	CHeap<int> maxHeap; // CHeap<int, Greater> minHeap;
 
 	for (int i = 0; i < v.size(); ++i)
 	{
-		heap.Insert(v[i]);
+		maxHeap.Insert(v[i]);
 	}
 
 	for (int i = 0; i < v.size(); ++i)
 	{
-		v[i] = heap.Delete();
+		v[i] = maxHeap.Delete();
 	}
 
 	PrintArray("▶ 정렬 후", v);
@@ -97,7 +114,7 @@ void PrintArray(const string& tag, const vector<int>& v)
 
 	for (int i = 0; i < v.size(); ++i)
 	{
-		cout << v[i] << " ";
+		cout << v[i] << ' ';
 	}
 
 	cout << endl << endl;
